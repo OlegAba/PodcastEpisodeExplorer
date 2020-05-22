e0f605520edb0ed0c953c5e0190fecb0156d0f22
Oleg Abalonski <o_abalonski@yahoo.com>
2020-05-22 19:09:18 -0400

    Initialization

---
+++ PodcastEpisodeExplorer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PodcastEpisodeExplorer
+//
+//  Created by Oleg Abalonski on 5/22/20.
+//  Copyright © 2020 Oleg Abalonski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
